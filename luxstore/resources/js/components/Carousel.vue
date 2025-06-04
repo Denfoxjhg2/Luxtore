6a4d5f68e1099235bae4b598febfe385e0cb5af4
Denfoxjhg2 <118984331+Denfoxjhg2@users.noreply.github.com>
2025-06-05 00:17:54 +0400

    adding content on the home page

---
+++ luxstore/resources/js/components/Carousel.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useCycleList } from '@vueuse/core'
+import router from '../src/router/router';
+
+
+const {state, next, prev} = useCycleList([
+    '/assets/ads/main.jpg',
+    '/assets/ads/iphone1.jpg',
+    '/assets/ads/iphone2.jpg',
+    '/assets/ads/pixel.jpg',
+    '/assets/ads/samsung.jpg',
+]);
+
+setInterval(() => { next() }, 6500);
+
+</script>
+<template>
+  <div class="relative h-[750px]">
+    <transition>
+        <img :src="state" alt="carousel" :key="state" @click="router.router.push('/catalog')" class="absolute object-cover cursor-pointer">
+    </transition>
+  </div>
+  <div class="h-16 bg-gradient-to-t from-slate-600 to-transparent relative items-center flex">
+    <div class="flex w-full justify-center gap-6">
+        <img @click="prev()" src="/assets/icons/carousel_next.svg" class="cursor-pointer rotate-180 transition hover:-translate-y-1 hover:shadow-xl">
+        <img @click="next()" src="/assets/icons/carousel_next.svg" class="cursor-pointer transition hover:-translate-y-1 hover:shadow-xl">
+    </div>
+  </div>
+</template>
+<style>
+.v-enter-active,
+.v-leave-active {
+    transition: all 1.5s ease;
+}
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
--- luxstore/resources/js/pages/Home.vue
+++ luxstore/resources/js/pages/Home.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
-
+    import Carousel from '@/components/Carousel.vue';
+    import router from '../src/router/router';
 </script>
 <template>
-    <div>Home</div>
+    <Carousel />
+    <div class="w-full h-64 flex justify-between">
+        <div class="bg-white w-1/4 flex justify-center items-center border border-slate-300 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl" @click="router.router.push('/catalog')">
+            <img src="/assets/company_ico/apple_black.svg" alt="apple">
+        </div>
+        <div class="bg-white w-1/4 flex justify-center items-center border border-slate-300 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl" @click="router.router.push('/catalog')">
+            <img src="/assets/company_ico/sams_black.svg" alt="samsung">
+        </div>
+        <div class="bg-white w-1/4 flex justify-center items-center border border-slate-300 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl" @click="router.router.push('/catalog')">
+            <img src="/assets/company_ico/google_black.svg" alt="google">
+        </div>
+        <div class="bg-white w-1/4 flex justify-center items-center border border-slate-300 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl" @click="router.router.push('/catalog')">
+            <img src="/assets/company_ico/noth_black.svg" alt="nothing">
+        </div>
+    </div>
+    <div>
+        <h1>Популярное только для вас.</h1>
+        
+    </div>
 </template>
