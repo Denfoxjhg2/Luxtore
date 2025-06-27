ef1e50d4969ac66a300849688c97df5d7a77a05c
Denfoxjhg2 <118984331+Denfoxjhg2@users.noreply.github.com>
2025-06-28 00:34:49 +0400

    NOW axios is ready, i think...

--- luxstore/resources/js/src/axios/axios.ts
+++ luxstore/resources/js/src/axios/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 if (import.meta.env.PROD) {
-    axios.defaults.baseURL = 'https://luxtore-production.up.railway.app';
+    axios.defaults.baseURL = 'https://luxtore-production.up.railway.app/api';
 } else {
-    axios.defaults.baseURL = 'http://127.0.0.1:8000';
+    axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 }
 const axiosInstance = axios.create({
     baseURL: axios.defaults.baseURL,
