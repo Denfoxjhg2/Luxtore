39a6f79ebc95c4cf8962c5315a7bcbeb0d139b14
Denfoxjhg2 <118984331+Denfoxjhg2@users.noreply.github.com>
2025-06-28 00:32:16 +0400

    axios modification

--- luxstore/resources/js/src/axios/axios.ts
+++ luxstore/resources/js/src/axios/axios.ts
@@ -1,9 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
 
+if (import.meta.env.PROD) {
+    axios.defaults.baseURL = 'https://luxtore-production.up.railway.app';
+} else {
+    axios.defaults.baseURL = 'http://127.0.0.1:8000';
+}
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: axios.defaults.baseURL,
     withCredentials: true,
-    withXSRFToken: true
+    withXSRFToken: true,
 });
 
 export default axiosInstance;
